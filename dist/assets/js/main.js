5a9752c4926a6e182c2f2447779f4f57984a5b6b
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-06-09 18:11:39 +0300

    Тест с слайдером

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -2,59 +2,11 @@
 	Пример записи подключения	//= components/script.js
 */
 
-const burger = document.querySelector('.burger');
-const burgerClose = document.querySelector('.burger__close');
-const burgerWrapper = document.querySelector('.burger__wrapper');
-const body = document.querySelector('body');
-
-burger.addEventListener('click', () => {
-	burger.classList.add('burger--open')
-	burgerWrapper.classList.add('burger__wrapper--open');
-	body.classList.add('_lock');
-})
-
-burgerClose.addEventListener('click', () => {
-	burger.classList.remove('burger--open');
-	burgerWrapper.classList.remove('burger__wrapper--open');
-	body.classList.remove('_lock');
-})
-
-console.log('hi');
-const swiper = new Swiper('.swiper', {
-	direction: 'horizontal',
-	slidesPerView: 1,
-	spaceBetween: 20,
-	// Navigation arrows
-	navigation: {
-		nextEl: '.process__slider-button-next',
-		prevEl: '.process__slider-button-prev',
-	},
-	breakpoints: {
-
-		740: {
-			slidesPerView: 2,
-		}
-
-	},
-});
-const factorySwiper = new Swiper('.factory__slider-swiper', {
-	direction: 'horizontal',
-	slidesPerView: 1,
-	spaceBetween: 20,
-	centeredSlidesBounds: true,
-	// Navigation arrows
-	navigation: {
-		nextEl: '.factory__slider-button-next',
-		prevEl: '.factory__slider-button-prev',
-	},
-	// breakpoints: {
-
-		// 740: {
-		// 	slidesPerView: 2,
-		// }
-
-	// },
-});
+/* //= components/var.js */
+/* //= components/function.j */
+/* //= components/header.js */
+/* //= components/process.js */
+/* //= components/factory.js */
 const team = new Swiper('.team__list-wrapper', {
 	direction: 'horizontal',
 	slidesPerView: 1,
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,9 +2,9 @@
 	Пример записи подключения	//= components/script.js
 */
 
-//= components/var.js
-//= components/function.js
-//= components/header.js
-//= components/process.js
-//= components/factory.js
+/* //= components/var.js */
+/* //= components/function.j */
+/* //= components/header.js */
+/* //= components/process.js */
+/* //= components/factory.js */
 //= components/team.js
