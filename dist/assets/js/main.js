708407a1de0528713fa39fb79b5dec5ecf069cd2
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-06-10 15:22:52 +0300

    Оптимизация слайдера

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -51,30 +51,69 @@ const hideCards = (cards, btnShow, maxCardsMath) => {
 }
 
 /**
+ * При изменении размера экрана меняет кнопку на "показать ещё",
+ * так как при изменении размера экрана галерея перерисовывается 
+ * и кнопка тоже должна
+*/
+const renderButtons = (btnShow, btnHide) => {
+	if (btnShow.classList.contains('hidden')) {
+		btnShow.classList.remove('hidden');
+	}
+	if (btnHide && !btnHide.classList.contains('hidden')) {
+		btnHide.classList.add('hidden');
+	}
+}
+
+/**
+ * При нажатии на кнопку(btnShow) - показывает все карточки
+ */
+const clickBtnShow = (cards, btnShow, btnHide) => {
+	btnShow.addEventListener('click', () => { 	
+		cards.forEach(item => item.classList.remove('hidden'));
+
+		btnShow.classList.add('hidden');
+		
+		if (btnHide) {
+			btnHide.classList.remove('hidden');
+		}
+	})
+}
+
+/**
+ * При нажатии на кнопку(btnHide) - скрывает лишние карточки
+ */
+const clickBtnHide = (cards, maxCardsMath, btnHide, btnShow) => {
+	btnHide.addEventListener('click', () => {
+		hideCards(cards, btnShow, maxCardsMath);
+
+		btnShow.classList.remove('hidden');
+		btnHide.classList.add('hidden');
+	})
+}
+
+/**
  * Выполнение проверки заданной в пределах ширин экрана: maxWidth и minWidth  
  * и отображение кнопки(btnShow) 'показать ещё',
  * если количество карточек(cards) больше задаваемого числа(maxCards)
+ * @param {array} cards 
+ * @param {number} maxCards 
+ * @param {const} btnShow 
+ * @param {number} maxWidth 
+ * @param {number} minWidth 
 **/
 const response = (cards, maxCards, btnShow, btnHide, minWidth, maxWidth) => {
 	let maxCardsMath = maxCards - 1;
+
+	renderButtons(btnShow, btnHide); // Рендер кнопок при изменении ширины экрана
+	
 	// Если ширина экрана входит в указанные размеры ширины
 	if (window.innerWidth > minWidth && window.innerWidth <= maxWidth) {
-		// Скрывает лишние карточки
-		hideCards(cards, btnShow, maxCardsMath);
-		
-    btnShow.addEventListener('click', () => { 	// При нажатии на кнопку(btnShow) - показать все карточки
-        cards.forEach(item => item.classList.remove('hidden'));
+		hideCards(cards, btnShow, maxCardsMath); 							// Скрывает лишние карточки
+		clickBtnShow(cards, btnShow, btnHide); 								// При нажатии на кнопку(btnShow) - показывает все карточки
 
-				btnShow.classList.add('hidden');
-				btnHide.classList.remove('hidden');
-			})
-
-			btnHide.addEventListener('click', () => { 	// При нажатии на кнопку(btnHide) - показать все карточки
-				hideCards(cards, btnShow, maxCardsMath);
-
-				btnShow.classList.remove('hidden');
-				btnHide.classList.add('hidden');
-			})
+		if (btnHide) {							
+			clickBtnHide(cards, maxCardsMath, btnHide, btnShow); 	// При нажатии на кнопку(btnHide) - скрывает лишние карточки
+		}					
 	}
 }
 
@@ -84,7 +123,7 @@ response(teamSlides, teamSlides.length, teamBtnShow, teamBtnHide, 320, 1110);
 
 // При изменении ширины (динамика) происходит расчёт карточек и показ(скрытие) кнопки
 window.addEventListener('resize', event => {
-	let eventWidth = event.target.window.innerWidth
+	let eventWidth = event.target.window.innerWidth; 
 
     if (eventWidth <= 1920 && eventWidth > 1110) response(teamSlides, 6, teamBtnShow, teamBtnHide, 1110, 1920);
     if (eventWidth <= 1110 && eventWidth > 320) response(teamSlides, teamSlides.length, teamBtnShow, teamBtnHide, 320, 1110);
